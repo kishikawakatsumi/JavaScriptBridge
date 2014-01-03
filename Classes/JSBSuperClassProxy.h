637b65831bacfad841b09ffa2f18c82464f22850
kishikawa katsumi <kishikawakatsumi@mac.com>
2014-01-04 05:59:18 +0900

    Update .travis.yml

---
+++ Classes/JSBSuperClassProxy.h
@@ -0,0 +1,13 @@
+//
+//  JSBSuperClassProxy.h
+//  JavaScriptBridge
+//
+//  Created by kishikawa katsumi on 2014/01/04.
+//  Copyright (c) 2014年 kishikawa katsumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSBSuperClassProxy : NSProxy
+
+@end
---
+++ Classes/JSBSuperClassProxy.m
@@ -0,0 +1,13 @@
+//
+//  JSBSuperClassProxy.m
+//  JavaScriptBridge
+//
+//  Created by kishikawa katsumi on 2014/01/04.
+//  Copyright (c) 2014年 kishikawa katsumi. All rights reserved.
+//
+
+#import "JSBSuperClassProxy.h"
+
+@implementation JSBSuperClassProxy
+
+@end
