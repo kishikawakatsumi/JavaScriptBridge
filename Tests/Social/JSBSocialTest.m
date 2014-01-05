e78716efe3bd53a27ab9aee07ec190fd87c66966
kishikawa katsumi <kishikawakatsumi@mac.com>
2014-01-05 19:50:11 +0900

    Refine tests.

--- Tests/Social/JSBSocialTest.m
+++ Tests/Social/JSBSocialTest.m
@@ -22,11 +22,12 @@
 - (void)testInstantiation
 {
     JSContext *context = [[JSContext alloc] init];
+    [context addScriptingSupport:@"Foundation"];
     [context addScriptingSupport:@"Social"];
 
     JSValue *value = nil;
     
-    [context evaluateScript:@"var slrequest = SLRequest.alloc();"];
+    [context evaluateScript:@"var slrequest = SLRequest.requestForServiceTypeRequestMethodURLParameters('com.apple.social.twitter', 0, NSURL.URLWithString('http://www.apple.com/'), null);"];
     value = context[@"slrequest"];
     XCTAssertTrue(value && !value.isUndefined);
 
